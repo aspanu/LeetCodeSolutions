cf10a854ea50c1e57b079f65b688593d3e9d434f
Adrian Spanu <adri@nspanu.ca>
2020-04-09 13:10:26 -0700

    Day 9 solution.

---
+++ src/30daychallenge/BackspaceCharacters.kt
@@ -0,0 +1,33 @@
+package `30daychallenge`
+
+/**
+ * Created by aspanu on 2020-04-09.
+ * https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/529/week-2/3291/
+ */
+class Solution9 {
+    fun backspaceCompare(S: String, T: String): Boolean {
+        val s = processInputString(S)
+        val t = processInputString(T)
+        return s == t
+    }
+
+    private fun processInputString(inputString: String): String {
+        var tempString = ""
+        for (character in inputString) {
+            tempString = if (character == '#') {
+                tempString.dropLast(1)
+            } else {
+                tempString.plus(character)
+            }
+        }
+        return tempString
+    }
+}
+
+fun main(args: Array<String>) {
+    val sol = Solution9()
+    println(sol.backspaceCompare("ab#c", "ad#c"))
+    println(sol.backspaceCompare("ab##", "c#d#"))
+    println(sol.backspaceCompare("a##c", "#a#c"))
+    println(sol.backspaceCompare("a#c", "b"))
+}
