cd2a7af444a6beb0693e5015f1650964c4936b1b
Adrian Spanu <adri@nspanu.ca>
2020-04-26 09:31:47 -0700

    Day 25 and 26

---
+++ src/30daychallenge/CanJump.kt
@@ -0,0 +1,27 @@
+package `30daychallenge`
+
+/**
+ * Created by aspanu on 2020-04-25.
+ * https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/531/week-4/3310/
+ */
+
+class Solution25 {
+    fun canJump(nums: IntArray): Boolean {
+        val size = nums.size
+        var maximumJumpIndex = 0
+
+        for (i in 0 until size) {
+            if (i > maximumJumpIndex) break
+
+            maximumJumpIndex = Math.max(maximumJumpIndex, i + nums[i])
+        }
+
+        return maximumJumpIndex >= size - 1
+    }
+}
+
+fun main(args: Array<String>) {
+    val sol = Solution25()
+    println(sol.canJump(intArrayOf(2,3,1,1,4)))
+    println(sol.canJump(intArrayOf(3,2,1,0,4)))
+}
---
+++ src/30daychallenge/LongestCommonSubsequence.kt
@@ -0,0 +1,38 @@
+package `30daychallenge`
+
+import kotlin.math.max
+
+/**
+ * Created by aspanu on 2020-04-26.
+ * https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/531/week-4/3311/
+ */
+
+class Solution26 {
+    fun longestCommonSubsequence(text1: String, text2: String): Int {
+        val longestSubsequenceSoFar = Array(text1.length) {IntArray(text2.length) {0} }
+        for (i in text1.indices) {
+            for (j in text2.indices) {
+                if (text1[i] == text2[j]) {
+                    if (i == 0 || j == 0) {
+                        longestSubsequenceSoFar[i][j] = 1
+                    } else {
+                        longestSubsequenceSoFar[i][j] = longestSubsequenceSoFar[i - 1][j - 1] + 1
+                    }
+                } else {
+                    val left = if (i == 0) 0 else longestSubsequenceSoFar[i-1][j]
+                    val up = if (j == 0) 0 else longestSubsequenceSoFar[i][j-1]
+                    longestSubsequenceSoFar[i][j] = max(left, up)
+                }
+            }
+        }
+        return longestSubsequenceSoFar.last().last()
+    }
+}
+
+fun main(args: Array<String>) {
+    val sol = Solution26()
+    println(sol.longestCommonSubsequence("abcde", "ace"))
+    println(sol.longestCommonSubsequence("abcde", "aec"))
+    println(sol.longestCommonSubsequence("abc", "abc"))
+    println(sol.longestCommonSubsequence("abc", "def"))
+}
